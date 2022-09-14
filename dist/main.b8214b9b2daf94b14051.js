7af1f01f748d75653605d6be923c10f9fde7bf63
irb1s <kolyirb1s@gmail.com>
2022-09-14 21:20:16 +0500

    Commit

--- dist/main.b8214b9b2daf94b14051.js
+++ dist/main.b8214b9b2daf94b14051.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "./src/index.ts":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./src/index.ts ***!
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://imes/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://iblog/./src/index.ts?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("\n\n//# sourceURL=webpack://imes/./src/index.js?");
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.ts"]();
 /******/ 	
 /******/ })()
 ;
