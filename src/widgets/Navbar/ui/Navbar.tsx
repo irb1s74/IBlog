f4ad212565a00cad8efbe6aa3240531e121172a7
irb1s <kolyirb1s@gmail.com>
2022-09-27 16:55:08 +0500

    Commit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,28 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames';
 import { Link } from 'shared/ui/Link/Link';
+import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-    <div className={classNames(styles.Navbar, {}, [className])}>
-        <div className={styles.content}>
-            <div className={styles.logo}>
-                IBlog
-            </div>
-            <div className={styles.actions}>
-                <div className={styles.links}>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(styles.Navbar, {}, [className])}>
+            <div className={styles.content}>
+                <div className={styles.logo}>
+                    {t('IBlog')}
+                </div>
+                <div className={styles.actions}>
+                    <div className={styles.links}>
+                        <Link to="/">{t('Домой')}</Link>
+                        <Link to="/about">{t('Информация')}</Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
