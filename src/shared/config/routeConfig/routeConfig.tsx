f508a31eed1fe92aff63265749a08eaa0feffa31
irb1s <kolyirb1s@gmail.com>
2022-09-29 14:32:29 +0500

    Commit

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { Main } from 'pages/Main';
 import { About } from 'pages/About';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'MAIN',
-    ABOUT = 'ABOUT'
+    ABOUT = 'ABOUT',
+    NOT_FOUND = 'NOT_FOUND'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath.ABOUT,
         element: <About />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.NOT_FOUND,
+        element: <NotFoundPage />,
     },
 };
